1e0c940a8c49d0f8e214ce2df8946bd18b31de01
hid_miy <miyaaaan@gmail.com>
2018-06-01 15:46:48 +0900

    fix line end

--- v0.4.0/bowling.js
+++ v0.4.0/bowling.js
@@ -1,12 +1,12 @@
 class Bowling {
   constructor() {
-    this.pins = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    this.current = 0
+    this.pins = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    this.current = 0;
   }
 
   roll(pins) {
-    this.pins[this.current] = pins
-    this.current++
+    this.pins[this.current] = pins;
+    this.current++;
   }
 
   isSpare(index) {
@@ -16,12 +16,12 @@ class Bowling {
   }
 
   score() {
-    var sum = 0
+    var sum = 0;
     var index = 0;
     for (var frame = 0; frame < 10; frame++) {
-      sum += this.pins[index] + this.pins[index + 1]
-      index += 2
+      sum += this.pins[index] + this.pins[index + 1];
+      index += 2;
     }
-    return sum
+    return sum;
   }
 }
