dcf7f3561daf8cb5125f4c852ffccc07fdc53f27
hid_miy <miyaaaan@gmail.com>
2018-06-01 14:14:49 +0900

    add v0.1.0, v0.5.0

--- v0.5.0/bowling-test.js
+++ v0.5.0/bowling-test.js
@@ -4,22 +4,24 @@ var expect = chai.expect;
 chai.Should();
 
 
+var sut = null;
+beforeEach(function() {
+  sut = new Bowling()
+})
+
 describe('Basic test', function() {
 
   it("all gutter, score is 0", function() {
-    var sut = new Bowling()
     sut.rollMany(0, 20)
     expect(sut.score()).to.eql(0);
   });
 
   it("1 roll 1pin, score is 1", function() {
-    var sut = new Bowling()
     sut.roll(1)
     expect(sut.score()).to.eql(1);
   });
 
   it("20 roll 1pin, score is 20", function() {
-    var sut = new Bowling()
     sut.rollMany(1, 20)
     expect(sut.score()).to.eql(20);
   });
@@ -29,25 +31,21 @@ describe('Basic test', function() {
 describe('Spare test', function() {
 
   it("if spare, score add after 1 roll", function() {
-    var sut = new Bowling()
     sut.rollByArray([5, 5, 1])
     expect(sut.score()).to.eql(12);
   });
 
   it("if spare, score add after 1 roll", function() {
-    var sut = new Bowling()
     sut.rollByArray([0, 10, 5])
     expect(sut.score()).to.eql(20);
   });
 
   it("two time spares", function() {
-    var sut = new Bowling()
     sut.rollByArray([0, 10, 5, 5, 4])
     expect(sut.score()).to.eql(33);
   });
 
   it("false spare", function() {
-    var sut = new Bowling()
     sut.rollByArray([2, 5, 5, 2])
     expect(sut.score()).to.eql(14);
   });
@@ -57,12 +55,12 @@ describe('Spare test', function() {
 describe('Strike test', function() {
 
   it("if Strike, score add after 2 roll", function() {
-    var sut = new Bowling()
     sut.rollByArray([2, 2, 10, 1, 2])
     expect(sut.score()).to.eql(20);
   });
 
 })
+
 
 mocha.checkLeaks();
 mocha.run();
